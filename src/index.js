c3c770194cb29a0de5bc43c248ccce9b906fdc11
shachem <sekki.hachem@outlook.com>
2022-09-04 15:48:22 +0100

    Connect the frontend to the firestore DB service

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import { initializeApp } from "firebase/app";
-
-const credentials = process.env;
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: credentials.apiKey,
-  authDomain: credentials.authDomain,
-  projectId: credentials.projectId,
-  storageBucket: credentials.storageBucket,
-  messagingSenderId: credentials.messagingSenderId,
-  appId: credentials.appId,
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
-
+// init the firebase app
 initializeApp(firebaseConfig);
+
+// init the firebase services
+const db = getFirestore();
+
+// reference the collection to read data from
+const collectionReference = collection(db, "books");
+
+// read the data from the collection
+getDocs(collectionReference).then((snapshot) => {
+  let books = [];
+  snapshot.docs.forEach((doc) => books.push({ ...doc.data(), id: doc.id }));
+  console.log(books);
+});
