9aa2329b847ba3015f7f9e51e9463c3263e8f489
shachem <sekki.hachem@outlook.com>
2022-09-05 07:25:56 +0100

    implement book addition and deletion forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.apiKey,
@@ -23,4 +30,26 @@ getDocs(collectionReference).then((snapshot) => {
   let books = [];
   snapshot.docs.forEach((doc) => books.push({ ...doc.data(), id: doc.id }));
   console.log(books);
+});
+
+// adding docs
+const addBookForm = document.querySelector(".add");
+addBookForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addDoc(collectionReference, {
+    title: addBookForm.title.value,
+    author: addBookForm.author.value,
+  }).then(() => {
+    addBookForm.reset();
+  });
+});
+
+// deleting docs
+const deleteBookForm = document.querySelector(".delete");
+deleteBookForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const documentToDelete = doc(db, "books", deleteBookForm.id.value);
+  deleteDoc(documentToDelete).then(() => {
+    deleteBookForm.reset();
+  });
 });
